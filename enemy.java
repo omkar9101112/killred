f7118545ce1dd132b12f607dacf0fb3579b47102
omkar9101112 <97611370+omkar9101112@users.noreply.github.com>
2022-02-09 00:48:37 -0800

    Add files via upload

---
+++ enemy.java
@@ -0,0 +1,6 @@
+public class enemy{
+    int x=1550;
+    int y=530;
+    int height = 200;
+    int width = 50;
+}
---
+++ enemyhealth.java
@@ -0,0 +1,7 @@
+public class enemyhealth {
+    int width =150;
+    int height = 30;
+    int y=200;
+    int x = 1500;
+}
+
